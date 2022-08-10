8fa1dbbab64cc27d6744a753cc17ddb58f07ff65
vilmal <vilma.liorensaityte@gmail.com>
2022-08-10 17:24:31 +0200

    OXDEV-5164 Run Unit tests

--- tests/Integration/Internal/Transition/Adapter/TemplateLogic/TranslateSalutationLogicTest.php
+++ tests/Integration/Internal/Transition/Adapter/TemplateLogic/TranslateSalutationLogicTest.php
@@ -5,19 +5,25 @@
  * See LICENSE file for license details.
  */
 
-namespace OxidEsales\EshopCommunity\Tests\Integration\Internal\Transition\Adapter\TemplateLogic;
+namespace OxidEsales\EshopCommunity\Tests\Unit\Internal\Transition\Adapter\TemplateLogic;
 
-use OxidEsales\Eshop\Core\Registry;
-use OxidEsales\EshopCommunity\Core\Language;
 use OxidEsales\EshopCommunity\Internal\Transition\Adapter\TemplateLogic\TranslateSalutationLogic;
-use OxidEsales\EshopCommunity\Internal\Transition\Adapter\Translator\LegacyTemplateTranslator;
-use OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase;
+use OxidEsales\TestingLibrary\UnitTestCase;
 
 /**
  * Class TranslateSalutationLogic
  */
-class TranslateSalutationLogicTest extends IntegrationTestCase
+class TranslateSalutationLogicTest extends UnitTestCase
 {
+
+    /** @var TranslateSalutationLogic */
+    private $translateSalutationLogic;
+
+    protected function setUp(): void
+    {
+        $this->translateSalutationLogic = new TranslateSalutationLogic();
+    }
+
     /**
      * Provides data for testTranslateSalutation
      *
@@ -42,20 +48,7 @@ class TranslateSalutationLogicTest extends IntegrationTestCase
      */
     public function testTranslateSalutation(string $ident, int $languageId, string $expected): void
     {
-        $translateSalutationLogic = new TranslateSalutationLogic($this->getTranslator($languageId));
-        $this->assertEquals($expected, $translateSalutationLogic->translateSalutation($ident));
-    }
-
-    /**
-     * @param $languageId
-     * @return LegacyTemplateTranslator
-     */
-    private function getTranslator($languageId)
-    {
-        $language = Registry::getLang();
-        $language->setTplLanguage($languageId);
-        $language->setAdminMode(false);
-        Registry::set(Language::class, $language);
-        return new LegacyTemplateTranslator();
+        $this->setLanguage($languageId);
+        $this->assertEquals($expected, $this->translateSalutationLogic->translateSalutation($ident));
     }
 }
